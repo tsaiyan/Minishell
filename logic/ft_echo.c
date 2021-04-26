d7d10ac4944672fad4392e4282abc3b5e6b82180
tsaiyan <tsaiyan21@yandex.ru>
2021-04-26 21:00:00 +0300

    echo working with multiply strs

--- logic/ft_echo.c
+++ logic/ft_echo.c
@@ -1,9 +1,28 @@
 #include "header.h"
 
-static int check_n(char *str)
+static void puts_echo(char **argv, int endn, int fd)
 {
 	int i;
 
+	i = 1;
+
+	while(argv[i])
+	{
+		ft_putstr_fd(argv[i], fd);
+		argv[i++];
+		if (argv[i])
+			ft_putchar_fd(32, fd);
+	}
+	if (endn)
+		ft_putchar_fd('\n', fd);
+}
+
+static int check_n(char **argv, int fd)
+{
+	int i;
+	char *str;
+
+	str = argv[1];
 	i = 2;
 	if (!ft_strncmp(str, "-n", 2))
 	{
@@ -11,7 +30,7 @@ static int check_n(char *str)
 		{
 			if (str[i] !=0 && str[i] != 'n')
 			{
-				ft_putstr_fd(str, 1);
+				puts_echo(argv, 1, fd);
 				return (2);
 			}
 			i++;
@@ -25,7 +44,9 @@ void ft_echo(char **argv)
 {
 	int flag;
 	int fd;
+	int i;
 
+	i = 0;
 	fd = 1;
 	if (argv[1] == 0)
 	{
@@ -33,16 +54,16 @@ void ft_echo(char **argv)
 		return;
 	}
 	ft_putstr_fd("\n", 1);
-	flag = check_n(argv[1]);
-	if (flag == 2 && argv[2])
+	flag = check_n(argv, fd);
+	if (flag == 2)
+		return;
+	i = flag;
+	while(argv[1 + flag])
 	{
-		ft_putstr_fd(" ", fd);
-		ft_putstr_fd(argv[2], fd);
-		return;
+		ft_putstr_fd(argv[1 + flag++], fd);
+		if (argv[1 + flag])
+			ft_putstr_fd(" ", fd);
 	}
-	else if (flag == 2)
-		return;
-	ft_putstr_fd(argv[1 + flag], fd);
-	if (flag != 1)
+	if (i != 1)
 		ft_putstr_fd("\n", fd);
 }
