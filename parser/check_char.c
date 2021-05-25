eef38190c9b29fce8425b66c6cfb0587d1b6dbbe
egums <dima110.ru@gmail.com>
2021-05-25 18:23:10 +0300

    ok

--- parser/check_char.c
+++ parser/check_char.c
@@ -11,12 +11,6 @@ int	check_back_slash(char *cur_arg, t_pars *pa, int i)
 	return (0);
 }
 
-//	if (pa->back_slash == 0)
-//	{
-//		cur_arg[i] = EMPTY_BACK_SLASH;
-//		pa->back_slash = 1;
-//	}
-
 int	check_quotes(char *cur_arg, t_pars *pa, int i)
 {
 	if (cur_arg[i] == S_QUOT && pa->quot_flag == 0)
@@ -42,6 +36,20 @@ int	check_quotes(char *cur_arg, t_pars *pa, int i)
 	return (0);
 }
 
+static int	check_pipes_in_pre(char **cur_arg, t_pars *pa, int *i)
+{
+	char	*del;
+
+	if (pa->quot_flag == 0)
+	{
+		del = *cur_arg;
+		*cur_arg = find_substr_in_str_and_replace("|", *cur_arg, " | ", i);
+		free(del);
+		*i += 2;
+	}
+	return (0);
+}
+
 int	check_char(char **cur_arg, t_pars *pa, int *i)
 {
 	if (cur_arg[0][*i] == '\\')
@@ -56,5 +64,7 @@ int	check_char(char **cur_arg, t_pars *pa, int *i)
 		check_envp(cur_arg, pa, i, STAGE_SECOND);
 	else if (cur_arg[0][*i] == ' ' && pa->quot_flag != 0)
 		cur_arg[0][*i] = EMPTY_SPACE;
+	else if (cur_arg[0][*i] == '|')
+		check_pipes_in_pre(cur_arg, pa, i);
 	return (0);
 }
