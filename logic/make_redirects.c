a13d9a899215ee44753a7a38829b199c710d22b1
tsaiyan <tsaiyan21@yandex.ru>
2021-05-27 18:27:30 +0300

    push

--- logic/make_redirects.c
+++ logic/make_redirects.c
@@ -1,5 +1,28 @@
 #include "header.h"
 
+int	it_not_redirect_str(char *str, int len)
+{
+	if (len)
+	{
+		if (!ft_strncmp(str, "\007", len))
+			return (0);
+		if (!ft_strncmp(str, "\007\007", len))
+			return (0);
+		if (!ft_strncmp(str, "\006", len))
+			return (0);
+	}
+	if (len == 0)
+	{	
+		if (!ft_strcmp(str, "\007"))
+			return (0);
+		if (!ft_strcmp(str, "\007\007"))
+			return (0);
+		if (!ft_strcmp(str, "\006"))
+			return (0);
+	}
+	return (1);
+}
+
 int	ft_strlcpy2(char *dest, const char *src, int start, int end)
 {
 	int	i;
@@ -26,14 +49,17 @@ void	make_new_str(t_bin *bin, int i, int j)
 	int	red_len;
 
 	red_len = type_of_redirect(bin->argv[i], j);
-	bin->com_to_ins = ft_calloc(sizeof(char *), j + 1);
-	ft_check_malloc(bin->com_to_ins);
 	bin->file_to_ins = ft_calloc(sizeof(char *), \
 	ft_strlen(bin->argv[i] - j) + 1);
 	ft_check_malloc(bin->file_to_ins);
 	bin->red_to_ins = ft_calloc(sizeof(char *), 4);
 	ft_check_malloc(bin->red_to_ins);
-	ft_strlcpy2(bin->com_to_ins, bin->argv[i], 0, j);
+	if (it_not_redirect_str(bin->argv[i], red_len))
+	{
+		bin->com_to_ins = ft_calloc(sizeof(char *), j + 1);
+		ft_check_malloc(bin->com_to_ins);
+		ft_strlcpy2(bin->com_to_ins, bin->argv[i], 0, j);
+	}
 	ft_strlcpy2(bin->file_to_ins, bin->argv[i], j + red_len, 0);
 	ft_strlcpy2(bin->red_to_ins, bin->argv[i], j, red_len);
 }
@@ -52,7 +78,8 @@ int	insert_thee_arrays(t_bin *bin, int change)
 		new_argv[i] = bin->argv[j];
 		j++;
 	}
-	new_argv[i++] = bin->com_to_ins;
+	if (bin->com_to_ins)
+		new_argv[i++] = bin->com_to_ins;
 	new_argv[i++] = bin->red_to_ins;
 	new_argv[i++] = bin->file_to_ins;
 	while (bin->argv[++j])
@@ -63,17 +90,6 @@ int	insert_thee_arrays(t_bin *bin, int change)
 	free(bin->argv[change]);
 	free(bin->argv);
 	bin->argv = new_argv;
-	return (1);
-}
-
-int	it_not_redirect_str(char *str)
-{
-	if (!ft_strcmp(str, "\007"))
-		return (0);
-	if (!ft_strcmp(str, "\007\007"))
-		return (0);
-	if (!ft_strcmp(str, "\006"))
-		return (0);
 	return (1);
 }
 
@@ -90,7 +106,7 @@ void	make_redirects(t_bin *bin)
 		flag = 0;
 		while (bin->argv[++i] && flag == 0)
 		{
-			if (!it_not_redirect_str(bin->argv[i]))
+			if (!it_not_redirect_str(bin->argv[i], 0))
 				continue ;
 			j = -1;
 			while (bin->argv[i][++j] && flag == 0)
